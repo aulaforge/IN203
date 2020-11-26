aaf1c7ea70803f813d9e44edf847a213b0ecf6d3
aulaforge <62746663+aulaforge@users.noreply.github.com>
2020-11-26 09:55:25 +0100

    Rename MandelbrotBase.cpp to td2/MandelbrotBase.cpp

--- td2/MandelbrotBase.cpp
+++ td2/MandelbrotBase.cpp
@@ -7,9 +7,9 @@
 # include <fstream>
 #include <mpi.h>
 
-/** Une structure complexe est définie pour la bonne raison que la classe
- * complex proposée par g++ est très lente ! Le calcul est bien plus rapide
- * avec la petite structure donnée ci--dessous
+/** Une structure complexe est dÃ©finie pour la bonne raison que la classe
+ * complex proposÃ©e par g++ est trÃ¨s lente ! Le calcul est bien plus rapide
+ * avec la petite structure donnÃ©e ci--dessous
  **/
 struct Complex
 {
@@ -35,21 +35,21 @@ std::ostream& operator << (std::ostream& out, const Complex& c)
     return out;
 }
 
-/** Pour un c complexe donné, calcul le nombre d'itérations de mandelbrot
- * nécessaires pour détecter une éventuelle divergence. Si la suite
+/** Pour un c complexe donnÃ©, calcul le nombre d'itÃ©rations de mandelbrot
+ * nÃ©cessaires pour dÃ©tecter une Ã©ventuelle divergence. Si la suite
  * converge, la fonction retourne la valeur maxIter
  **/
 int iterMandelbrot(int maxIter, const Complex& c)
 {
     Complex z{ 0.,0. };
-    // On vérifie dans un premier temps si le complexe
-    // n'appartient pas à une zone de convergence connue :
+    // On vÃ©rifie dans un premier temps si le complexe
+    // n'appartient pas Ã  une zone de convergence connue :
     // Appartenance aux disques  C0{(0,0),1/4} et C1{(-1,0),1/4}
     if (c.real * c.real + c.imag * c.imag < 0.0625)
         return maxIter;
     if ((c.real + 1) * (c.real + 1) + c.imag * c.imag < 0.0625)
         return maxIter;
-    // Appartenance à la cardioïde {(1/4,0),1/2(1-cos(theta))}    
+    // Appartenance Ã  la cardioÃ¯de {(1/4,0),1/2(1-cos(theta))}    
     if ((c.real > -0.75) && (c.real < 0.5)) {
         Complex ct{ c.real - 0.25,c.imag };
         double ctnrm2 = sqrt(ct.sqNorm());
@@ -66,19 +66,19 @@ int iterMandelbrot(int maxIter, const Complex& c)
 
 /**
  * On parcourt chaque pixel de l'espace image et on fait correspondre par
- * translation et homothétie une valeur complexe c qui servira pour
- * itérer sur la suite de Mandelbrot. Le nombre d'itérations renvoyé
+ * translation et homothÃ©tie une valeur complexe c qui servira pour
+ * itÃ©rer sur la suite de Mandelbrot. Le nombre d'itÃ©rations renvoyÃ©
  * servira pour construire l'image finale.
 
- Sortie : un vecteur de taille W*H avec pour chaque case un nombre d'étape de convergence de 0 à maxIter
+ Sortie : un vecteur de taille W*H avec pour chaque case un nombre d'Ã©tape de convergence de 0 Ã  maxIter
  MODIFICATION DE LA FONCTION :
- j'ai supprimé le paramètre W étant donné que maintenant, cette fonction ne prendra plus que des lignes de taille W en argument.
+ j'ai supprimÃ© le paramÃ¨tre W Ã©tant donnÃ© que maintenant, cette fonction ne prendra plus que des lignes de taille W en argument.
  **/
 void
 computeMandelbrotSetRow(int W, int H, int maxIter, int num_ligne, int* pixels)
 {
-    // Calcul le facteur d'échelle pour rester dans le disque de rayon 2
-    // centré en (0,0)
+    // Calcul le facteur d'Ã©chelle pour rester dans le disque de rayon 2
+    // centrÃ© en (0,0)
     double scaleX = 3. / (W - 1);
     double scaleY = 2.25 / (H - 1.);
     //
@@ -158,7 +158,7 @@ int main(int argc, char* argv[])
 {
     const int W = 800;
     const int H = 600;
-    // Normalement, pour un bon rendu, il faudrait le nombre d'itérations
+    // Normalement, pour un bon rendu, il faudrait le nombre d'itÃ©rations
     // ci--dessous :
     //const int maxIter = 16777216;
     const int maxIter = 8 * 65536;
